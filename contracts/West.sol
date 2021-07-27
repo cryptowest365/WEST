64667779a1817cd5196a558f0f7d7b25181d8eb1
cryptowest365 <cryptowest365@protonmail.com>
2021-07-27 21:57:50 +0800

    CERTIK Recommend update

--- contracts/West.sol
+++ contracts/West.sol
@@ -31,7 +31,7 @@ library SafeMath {
 contract ERC20Basic {
     function totalSupply() public view returns (uint256);
     function balanceOf(address _who) public view returns (uint256);
-    function transfer(address _to, uint256 _value) public returns (bool);
+    function transfer(address _to, uint256 _value) external returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
@@ -63,10 +63,9 @@ contract ERC20 is ERC20Basic {
     function allowance(address _owner, address _spender)
     public view returns (uint256);
 
-    function transferFrom(address _from, address _to, uint256 _value)
-    public returns (bool);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
 
-    function approve(address _spender, uint256 _value) public returns (bool);
+    function approve(address _spender, uint256 _value) external returns (bool);
     event Approval(
         address indexed owner,
         address indexed spender,
